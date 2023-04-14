dc1375de89bf829ce96015c732ff2c2534f3caf1
yorhs_malasquez <yorhs1688@gmail.com>
2023-04-14 00:52:26 -0500

    Agregando el select al formulario

--- src/componentes/formulario/formulario.js
+++ src/componentes/formulario/formulario.js
@@ -1,5 +1,6 @@
 import "./formulario.css";
 import CampoTexto from "../campo-texto";
+import ListaOpciones from "../lista-opciones";
 //props son datos que vamos a enviar a nuestros componentes
 const Formulario = ()=>{
     return <section className="formulario">
@@ -8,6 +9,7 @@ const Formulario = ()=>{
             <CampoTexto titulo="Nombre" placeholder="Ingresar Nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingresar Puesto"/>
             <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
+            <ListaOpciones/>
         </form>
     </section>
 }
---
+++ src/componentes/lista-opciones/index.js
@@ -0,0 +1,27 @@
+import "./listaopciones.css"
+const ListaOpciones = ()=>{
+    //para recorrer el arreglo se usa map
+    //Metodo Map -> arreglo.map((equipo,index) => {
+    //    return <option></option>
+    //})
+    const equipos = [
+        "Programación",
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX y Diseño",
+        "Móvil",
+        "Innovación y Gestión"
+    ]
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select>
+            { equipos.map( (equipo, index) => {
+                return <option key={index}>{equipos}</option>
+            })}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
